d48284319e4cf5924b1f4d44993b68d0429b9831
motoki-shun <motoki-shun@kayac.com>
2016-12-18 22:37:13 +0900

    create option: PURE_DISABLE_SET_TITLE

--- pure.zsh
+++ pure.zsh
@@ -109,7 +109,7 @@ prompt_pure_preexec() {
 	prompt_pure_cmd_timestamp=$EPOCHSECONDS
 
 	# shows the current dir and executed command in the title while a process is active
-	prompt_pure_set_title 'ignore-escape' "$PWD:t: $2"
+	[[ $PURE_DISABLE_SET_TITLE ]] && $PURE_DISABLE_SET_TITLE || prompt_pure_set_title 'ignore-escape' "$PWD:t: $2"
 }
 
 # string length ignoring ansi escapes
